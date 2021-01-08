b2d5922f76059cf1659bdbfa743c9ab876d6b6e0
Avijit Karmaker <ajkacca@gmail.com>
2021-01-08 12:04:28 +0200

    add linters

--- webpack.config.js
+++ webpack.config.js
@@ -9,36 +9,36 @@ module.exports = {
   devServer: {
     contentBase: path.join(__dirname, 'dist'),
     compress: true,
-    port: 9000
+    port: 9000,
   },
-  mode: "development",
+  mode: 'development',
   module: {
- rules: [
-    {
-     test: /\.css$/i,
-     use: ['style-loader', 'css-loader'],
-     },
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
 
-     {
-       test: /\.(png|svg|jpg|jpeg|gif)$/i,
-       use: [
-         'file-loader',
-       ],
-     },
-     {
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        use: [
+          'file-loader',
+        ],
+      },
+      {
 			    test: /\.(ogg|mp3|wav|mpe?g)$/i,
-			    use: 'file-loader'
-			},
-     {
-    test: /\.m?js$/,
-    exclude: /(node_modules|bower_components)/,
-    use: {
-      loader: 'babel-loader',
-      options: {
-        presets: ['@babel/preset-env']
-      }
-    }
-  }
-           ],
-         }
+			    use: 'file-loader',
+      },
+      {
+        test: /\.m?js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+          },
+        },
+      },
+    ],
+  },
 };
