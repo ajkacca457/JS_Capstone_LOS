b316849c93d6bb9005bc7ab7fc686efbd39fc337
Avijit Karmaker <ajkacca@gmail.com>
2021-01-08 12:20:05 +0200

    add and initialize phaser

--- src/index.js
+++ src/index.js
@@ -1,1 +1,22 @@
-console.log('hello');
+import Phaser from "phaser";
+import Game from "./game";
+import Game2 from "./game2";
+import Preload from "./preload";
+import './style.css';
+
+const config= {
+
+  type: Phaser.AUTO,
+  width:800,
+  height:500,
+  physics:{
+    default: "arcade",
+    arcade: {
+      gravity:{ y:300 },
+      debug:true
+    }
+  },
+  scene:[Preload,Game,Game2],
+}
+
+export default new Phaser.Game(config)
